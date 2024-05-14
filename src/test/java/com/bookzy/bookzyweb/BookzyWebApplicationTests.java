4c1cfb8a463c0d2d3c28ce51de4c4ead39d7d38b
arcotpavankumar1411 <arcotpavankumar1411@gmail.com>
2024-05-14 12:21:24 +0000

    adding bookzy file

---
+++ src/test/java/com/bookzy/bookzyweb/BookzyWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookzy.bookzyweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookzyWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
